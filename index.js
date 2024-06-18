0203c5fc60866dc05ec65ed3b946fcf4fd62ff3c
Elias <eliasmoukarzel12@gmail.com>
2024-06-18 10:51:22 +0300

    Add index.js file to setup Express server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Route for serving index.html (entry point of your Vue.js app)
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+// Listen on port
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
