0abbcba78f5beeb3d77221d6aae0db2b355b7d63
CodingAdventuror <warningamer1@gmail.com>
2024-06-20 15:00:06 +0300

    Fixing deploy 4

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://your-render-app-url/api' : 'http://localhost:3001',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://gymfinal-1.onrender.com/api' : 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
   },
